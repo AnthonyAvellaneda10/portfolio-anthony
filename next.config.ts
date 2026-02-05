8aaca109d81680b685652a0f3874502493e5d222
Anthony <anthony.avellaneda@inmater.pe>
2026-02-05 12:00:22 -0500

    feat(i18n): setup routing and request configuration

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  // images: {
-  //   domains: ["i.ibb.co", "acortar.link"],
-  // },
   images: {
     remotePatterns: [
       {
@@ -18,7 +18,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  /* config options here */
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
